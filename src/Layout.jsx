2f477bed641bcc8837e81a9bc5086b75a788dcf9
RAJ <singhrajtilak64@gmail.com>
2024-06-19 13:54:32 +0530

    bg-change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -32,8 +32,8 @@ const Layout = () => {
             data-scroll-container
             ref={scrollRef}
             id="main"
-            className="bg-black"
-            style={{ backgroundColor: "black" }}
+            className=" bg-transparent"
+           
           >
             <Navbar />
             <Outlet />
